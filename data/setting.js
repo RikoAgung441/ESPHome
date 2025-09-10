d01d9dd5e94e4694deb362d233f6a46f5c06e050
dewanggaMz <dewanggamahfuz56@gmail.com>
2025-09-10 19:07:18 +0700

    func get data setting and toast

--- data/setting.js
+++ data/setting.js
@@ -1,49 +1,73 @@
 const buttonEye = document.querySelector(".input_icon_eye")
 const inputSSID = document.querySelector("#SSID")
-const inputPassword = document.querySelector("#input_wifi_password")
-// const btnSubmit = document.querySelector(".btn_submit")
-
-buttonEye.addEventListener("click", () => {
-	inputPassword.type = inputPassword.type === "password" ? "text" : "password"
-	buttonEye.src =
-		inputPassword.type === "password"
-			? "./asset/eye_off.svg"
-			: "./asset/eye_show.svg"
-})
-
+const inputPassword = document.querySelector("#wifi_password")
 const fields = document.querySelectorAll(".container_field_form")
+
 fields.forEach((field) => {
 	field.querySelector(".btn_set").addEventListener("click", async () => {
 		const key = field.querySelector("input").dataset.key
 		const value = field.querySelector("input").value
 
-		const res = await fetch("/save", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ key, value }),
-		})
+		try {
+			const res = await fetch("/save", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ key, value }),
+			})
 
-		const data = await res.json()
+			const data = await res.json()
 
-		if (data.status === "success") {
-			alert("Success")
+			if (data.status === "success") {
+				showToast("Set successfully!")
+			}
+		} catch (error) {
+			showToast("Failed to set data", true)
 		}
-		console.log(data)
 	})
 })
 
+function showToast(message, error = false, duration = 3000) {
+	const container = document.getElementById("toast-container")
+
+	const toast = document.createElement("div")
+	toast.className = "toast"
+	toast.innerText = message
+	container.appendChild(toast)
+
+	setTimeout(() => {
+		error && toast.classList.add("error")
+		toast.classList.add("show")
+	}, 100)
+
+	setTimeout(() => {
+		toast.classList.remove("show")
+		toast.classList.add("hide")
+
+		toast.addEventListener("transitionend", () => toast.remove())
+	}, duration)
+}
+
+const getData = async () => {
+	try {
+		const res = await fetch("/api/settings", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		})
+		const data = await res.json()
+		console.log(data)
+	} catch (error) {
+		showToast("Failed to fetch data from server", true)
+	}
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
-	const res = await fetch("/api/test", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({}),
+	getData()
+
+	buttonEye.addEventListener("click", () => {
+		inputPassword.type = inputPassword.type === "password" ? "text" : "password"
+		buttonEye.src =
+			inputPassword.type === "password"
+				? "./asset/eye_off.svg"
+				: "./asset/eye_show.svg"
 	})
-	const data = await res.json()
-	console.log(data)
-	// inputSSID.value = data.ssid
-	// inputPassword.value = data.password
-	// document.querySelector("#wifi_name").textContent = data.ssid
-	// document.querySelector("#wifi_password").textContent = data.password
-	// document.querySelector("#wifi_status").textContent = data.status
-	// document.querySelector("#wifi_ip").textContent = data.ip
 })
